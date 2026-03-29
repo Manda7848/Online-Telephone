a67b115a01865ca4dbc0498c507b95f94a9244fc
Manda7848 <manda7848code@gmail.com>
2026-03-29 22:10:52 +0100

    built a lobby

--- src/game.js
+++ src/game.js
@@ -1,5 +1,11 @@
 window.onload = () => {
 
+document.getElementById("join").onclick = () => {
+const player = document.getElementById("name").value.trim();
+const room = document.getElementById("room").value.trim();
+console.log(player,room);
+}
+
 
 
 
