3dd93f39cfe8c2b1e1880a6974babcf397991af0
Manda7848 <manda7848code@gmail.com>
2026-04-27 14:04:23 +0100

    it makes an id

--- src/game.js
+++ src/game.js
@@ -1,12 +1,26 @@
 window.onload = () => {
 
-document.getElementById("join").onclick = () => {
-const player = document.getElementById("name").value.trim();
-const room = document.getElementById("room").value.trim();
-console.log(player,room);
+// document.getElementById("join").onclick = () => {
+// const player = document.getElementById("name").value.trim();
+// const room = document.getElementById("room").value.trim();
+// console.log(player,room);
+// }
+
+
+
+
+let id = 0
+ const join = document.getElementById("join");
+
+join.onclick = console.log("Join was clicked");
+join.onclick = generateid;
+
+
+function generateid() {
+id = Math.floor(Math.random() * 500) + 1000;
+console.log("Your room id is =", id);
+document.getElementById("id").innerText = id;
+
+}
 }
 
-
-
-
-}
